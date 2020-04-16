d0c146a05267a43fd7663a1523c8f353388f4003
admin rat <adminrat@officex88cloudtropescom.local>
2020-04-16 16:20:12 -0700

    Update names files and add middle name

--- random_username.py
+++ random_username.py
@@ -1,10 +1,10 @@
 import random
 import os
 
-thing1 = random.choice(open('filename.txt').read().split()).strip()
-thing2 = random.choice(open('filename.txt').read().split()).strip()
-thing3 = random.choice(open('filename.txt').read().split()).strip()
+firstname = random.choice(open('firstnames.txt').read().split()).strip()
+middlename = random.choice(open('firstnames.txt').read().split()).strip()
+lastname = random.choice(open('lastnames.txt').read().split()).strip()
 
-username = thing1 + '_' + thing2 + '_' +thing3
+username = firstname + '_' + middlename + '_' + lastname
 
-print username
+print (username)
